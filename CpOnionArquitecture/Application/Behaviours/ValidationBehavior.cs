0975b82be2f8e19e2cb495130650a412a49aaf22
Juan Rodas <juan.rodas.manez@gmail.com>
2022-10-14 00:13:48 -0500

    feat: Avance Structure

---
+++ CpOnionArquitecture/Application/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Behaviours
+{
+    public class ValidationBehavior<IRequest, IResponse> : IPipelineBehavior<IRequest, IResponse> where IRequest : IRequest<IResponse>
+    {
+        private readonly IEnumerable<IValidator<IRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<IRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<IResponse> Handle(IRequest request, RequestHandlerDelegate<IResponse> next, CancellationToken cancellationToken)
+        {
+            if(_validators.Any())
+            {
+                var context = new FluentValidation.ValidationContext<IRequest>(request);
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if(failures.Count != 0)
+                {
+                    throw new Exceptions.ValidationException();
+                }
+            }
+            return await next();
+        }
+    }
+}
---
+++ CpOnionArquitecture/Application/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddAplicationLayer(this IServiceCollection services)
+        {
+            // Definiendo Extensions de Paquetes de Dependencia
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
